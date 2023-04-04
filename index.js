dcaec188c190db5cfcda93c0ef24bd8af626ff51
Matadorz350 <kljatva1983@gmail.com>
2023-04-04 13:16:38 +0500

    update at of all

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-let countEl = document.getElementById("count-el");
-let saveEl = document.getElementById("save-el");
-let count = 0;
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
 
 function increment() {
-  count += 1;
-  countEl.textContent = count;
+    count += 1
+    countEl.textContent = count
 }
 
 function save() {
-  let countStr = count + " - ";
-  saveEl.textContent += countStr;
-  count = 0;
-  countEl.textContent = count;
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
 }
-
-
